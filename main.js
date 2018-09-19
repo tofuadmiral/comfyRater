3c815b4e488fac6f679398e621a837aca697892a
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-09-19 11:09:30 -0400

    added some code into main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+
+// cat object definition
+
+var myCat = {
+    "name": "catting", 
+    "species:": "cat",
+    "favFood": "catfood"
+}
+
+myCat.favFood // gives you the value "catfood"
+
+
+// now exploring arrays
+var myFavColors = ["blue", "black", "white"];
+
+myFavColors[1]; // access "black"
+
+// now array of objects THIS IS A JSON
+var thePets = [
+    {
+        "name": "catting", 
+        "species:": "cat",              // JSON is javascript object notation  
+        "favFood": "catfood"            // so we have arrays nested in objects & vice versa
+    },
+    {
+        "name": "dogting", 
+        "species:": "dog",
+        "favFood": "chocochipcookies"
+    }
+]
